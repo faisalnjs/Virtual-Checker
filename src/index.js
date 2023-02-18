b4cbbfbe0ca25bbd7e4432163dcf27b979286565
Kenny <101839505+khui0@users.noreply.github.com>
2023-02-17 23:07:49 -0500

    More UI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
 import * as ui from "./ui.js";
 
-document.getElementById("settings").addEventListener("click", () => {
-    ui.alert("test", "hello");
+const modals = {
+    "code": () => {
+        ui.show(document.getElementById("code-modal"), "Seat Code", [
+            new ui.ModalButton("Cancel", true),
+            new ui.ModalButton("Save", false, () => {
+                console.log("Seat code save")
+            }),
+        ]);
+    },
+    "code-help": () => {
+        ui.show(document.getElementById("code-help-modal"), "Seat Code", [
+            new ui.ModalButton("Close", true)
+        ]);
+    },
+    "settings": () => {
+        ui.show(document.getElementById("settings-modal"), "Settings", [
+            new ui.ModalButton("Close", true)
+        ]);
+    },
+    "shortcuts": () => {
+        ui.show(document.getElementById("history-modal"), "Shortcuts", [
+            new ui.ModalButton("Close", true)
+        ]);
+    },
+    "theme": () => {
+        ui.show(document.getElementById("history-modal"), "Theme", [
+            new ui.ModalButton("Close", true)
+        ]);
+    },
+    "data": () => {
+        ui.show(document.getElementById("history-modal"), "Data", [
+            new ui.ModalButton("Close", true)
+        ]);
+    },
+    "history": () => {
+        ui.show(document.getElementById("history-modal"), "History", [
+            new ui.ModalButton("Close", true)
+        ]);
+    },
+}
+
+document.querySelectorAll("[data-show-modal]").forEach(button => {
+    button.addEventListener("click", e => {
+        modals[e.target.getAttribute("data-show-modal")]();
+    });
 });
