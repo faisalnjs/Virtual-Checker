49a7b5b2e321b7ddcd146a9cc968220c007a55de
Kenny <101839505+khui0@users.noreply.github.com>
2023-02-20 11:16:42 -0500

    Increment version

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const storage = new Storage("virtual-clicker-2");
 
 // Version
 
-const VERSION = "2.0";
+const VERSION = "2.0.9";
 document.querySelectorAll("span.version").forEach(element => {
     element.innerHTML = VERSION;
 });
