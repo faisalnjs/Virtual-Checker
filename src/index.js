b6b8ad87566577ba6f76a12eec64214089fdb1cf
Kenny <101839505+khui0@users.noreply.github.com>
2023-04-12 16:08:37 -0400

    2.1.6

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const storage = new Storage("virtual-clicker-2");
 
 // Version
 
-const VERSION = "2.1.5";
+const VERSION = "2.1.6";
 document.querySelectorAll("span.version").forEach(element => {
     element.innerHTML = VERSION;
 });
