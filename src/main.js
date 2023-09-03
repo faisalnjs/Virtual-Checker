e23a381216565b05a1a3a119143e98bfeca48f6d
Kenny <101839505+khui0@users.noreply.github.com>
2023-09-03 12:27:55 -0400

    Separate submit function

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "./themes.css";
 import "remixicon/fonts/remixicon.css";
 
 import * as ui from "./ui.js";
+import { submitClick } from "./clicker.js";
 import { Storage } from "./storage.js";
 import { Autocomplete } from "./autocomplete.js";
 
@@ -178,23 +179,6 @@ questionInput.addEventListener("input", e => {
 answerInput.addEventListener("input", e => {
     e.target.classList.remove("attention");
 });
-
-function submitClick(code, question, answer) {
-    const fields = {
-        "entry.1896388126": code,
-        "entry.1232458460": question,
-        "entry.1065046570": answer
-    }
-    const params = new URLSearchParams(fields).toString();
-    const url = "https://docs.google.com/forms/d/e/1FAIpQLSfwDCxVqO2GuB4jhk9iAl7lzoA2TsRlX6hz052XkEHbLrbryg/formResponse?";
-    fetch(url + params, {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-    });
-}
 
 function resetInputs() {
     questionInput.value = "";
