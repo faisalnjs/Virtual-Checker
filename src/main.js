42a4055dcb7a3e6cb043c2432d5a384c3e3dfc93
Kenny <101839505+khui0@users.noreply.github.com>
2023-09-05 22:47:14 -0400

    3.0.1

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const storage = new Storage("virtual-clicker-2");
 
 // Version
 
-const VERSION = "3.0.0";
+const VERSION = "3.0.1";
 document.querySelectorAll("span.version").forEach(element => {
     element.innerHTML = VERSION;
 });
