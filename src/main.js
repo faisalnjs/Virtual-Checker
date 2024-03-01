4f2bc5c57c31d383e41d73b6d23acfdd349d40e6
Kenny <101839505+khui0@users.noreply.github.com>
2024-02-29 22:13:28 -0500

    3.6.1

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import "/src/festive/festive.js";
 
 import storage from "/src/modules/storage.js";
 
-const VERSION = "3.6.0";
+const VERSION = "3.6.1";
 updateVersionString();
 function updateVersionString() {
     document.querySelectorAll("span.version").forEach(element => {
