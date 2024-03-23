c8bb3dfe3670f2a45deaa006d1725ab4bb6e4a77
Kenny <101839505+khui0@users.noreply.github.com>
2024-03-23 18:30:01 -0400

    Layout improvements 2

--- src/modules/mathlive.js
+++ src/modules/mathlive.js
@@ -1,8 +1,8 @@
 import "/node_modules/mathlive/dist/mathlive-fonts.css";
 import { MathfieldElement } from "mathlive";
 
-import * as ui from "/src/modules/ui.js";
+// import * as ui from "/src/modules/ui.js";
 
 MathfieldElement.soundsDirectory = null;
 
-ui.addTooltip(document.getElementById("answer-mode-selector"), "BETA: Features may not work as expected");
+// ui.addTooltip(document.getElementById("answer-mode-selector"), "BETA: Features may not work as expected");
