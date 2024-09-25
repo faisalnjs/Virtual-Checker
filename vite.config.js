d19d390cb9354cb245507798e2bd583fd239b664
Faisal N <contact@faisaln.com>
2024-09-25 15:57:54 -0400

    add admin to build

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import version from "vite-plugin-package-version";
 
 export default {
   plugins: [webfontDownload(), version()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: new URL('index.html', import.meta.url).pathname,
+        nested: new URL('admin/index.html', import.meta.url).pathname,
+      },
+    },
+  },
 };
