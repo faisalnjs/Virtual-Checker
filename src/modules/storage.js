8a4c4bb10e53d515266411d87e5120bc1ca5c85a
Faisal N <contact@faisaln.com>
2024-10-11 14:12:36 +0000

    sync storage

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,12 +1,14 @@
 class Storage {
   constructor(id) {
     this.id = id;
+    this.syncWithCookie();
   }
 
   set(key, value) {
     let temp = this.object;
     temp[key] = value;
     localStorage.setItem(this.id, JSON.stringify(temp));
+    setCookie(this.id, JSON.stringify(temp), 7);
     return this;
   }
 
@@ -22,16 +24,46 @@ class Storage {
     let temp = this.object;
     delete temp[key];
     localStorage.setItem(this.id, JSON.stringify(temp));
+    setCookie(this.id, JSON.stringify(temp), 7);
     return this;
   }
 
   obliterate() {
     localStorage.removeItem(this.id);
+    setCookie(this.id, "", -1);
   }
 
   get object() {
     return JSON.parse(localStorage.getItem(this.id)) || {};
   }
+
+  syncWithCookie() {
+    const cookieData = getCookie(this.id);
+    if (cookieData) {
+      localStorage.setItem(this.id, cookieData);
+    }
+  }
 }
 
-export default new Storage("virtual-checker-2");
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/; domain=.vssfalcons.com";
+}
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+export default new Storage("virtual-falcons-2");
