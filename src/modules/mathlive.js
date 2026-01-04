c138a22f29099f9b5c144e222ce61f5ae32c3055
Faisal N <contact@faisaln.com>
2026-01-04 00:18:35 -0500

    Bug fixes

--- src/modules/mathlive.js
+++ src/modules/mathlive.js
@@ -1,15 +1,4 @@
-import * as ui from "/src/modules/ui.js";
-import storage from "/src/modules/storage.js";
 import "/node_modules/mathlive/dist/mathlive-fonts.css";
 import { MathfieldElement } from "mathlive";
 
-try {
-  MathfieldElement.soundsDirectory = null;
-} catch (error) {
-  if (storage.get("developer")) {
-    alert(`Error @ mathlive.js: ${error.message}`);
-  } else {
-    ui.reportBugModal(null, String(error.stack));
-  }
-  throw error;
-};
+MathfieldElement.soundsDirectory = null;
