369dd1643618f76332257ada5ae2443df54624e9
Faisal N <contact@faisaln.com>
2026-01-08 19:37:21 -0500

    Segment Editor Create/Save and Exit button Fixes #486

--- src/checker/admin.js
+++ src/checker/admin.js
@@ -1100,6 +1100,9 @@ try {
   // Save
   document.querySelectorAll("#save-button").forEach(w => w.addEventListener("click", save));
   document.querySelectorAll("#create-button").forEach(w => w.addEventListener("click", createSegment));
+  document.querySelectorAll("#create-and-exit-button").forEach(w => w.addEventListener("click", () => {
+    createSegment(null, true);
+  }));
 
   async function save(event, hideResult) {
     if (!active) return;
@@ -3783,7 +3786,7 @@ try {
     ui.reloadUnsavedInputs();
   }
 
-  function createSegment() {
+  function createSegment(event = null, exitToCourse = false) {
     if (!active) return;
     const course = document.getElementById("course-period-input");
     const number = document.getElementById("segment-number-input");
@@ -3803,6 +3806,7 @@ try {
       return name.focus();
     }
     document.querySelector("#create-button").disabled = true;
+    document.querySelector("#create-and-exit-button").disabled = true;
     const question_ids = JSON.stringify(Array.from(document.querySelectorAll('.question')).filter(q => (q.querySelectorAll('input')[1].value.length > 0) && (q.querySelectorAll('input')[1].value != ' ')).map(q => {
       return {
         name: q.querySelectorAll('input')[1].value,
@@ -3848,7 +3852,11 @@ try {
       .then((r) => {
         ui.setUnsavedChanges(false);
         ui.toast(loadedSegmentEditor ? "Segment updated successfully." : "Segment created successfully.", 3000, "success", "bi bi-check-circle-fill");
-        editSegment(null, loadedSegmentEditor ? loadedSegment.id : (r.id || null));
+        if (exitToCourse) {
+          window.location.href = '/admin/';
+        } else {
+          editSegment(null, loadedSegmentEditor ? loadedSegment.id : (r.id || null));
+        }
       })
       .catch((e) => {
         console.error(e);
@@ -3889,6 +3897,7 @@ try {
     document.getElementById("question-list").innerHTML = '';
     JSON.parse(loadedSegment.question_ids).filter((item, index, self) => index === self.findIndex((t) => (t.id === item.id))).forEach(q => addExistingQuestion(q.id));
     document.getElementById("create-button").innerText = "Save";
+    document.getElementById("create-and-exit-button").innerText = "Save and Exit";
     document.querySelector('[data-delete-segment]')?.addEventListener('click', deleteSegmentConfirm);
     document.querySelector('[edit-segment-questions]')?.addEventListener('click', () => {
       if (ui.unsavedChanges) return ui.toast("You have unsaved changes. Please save or discard them before editing questions.", 3000, "error", "bi bi-exclamation-triangle-fill");
