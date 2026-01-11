4b983a72f53b4813f874f0ff2b4760db1c71d832
Faisal N <contact@faisaln.com>
2026-01-11 15:59:37 -0500

    Color-coded course mastery status segments Fixes #491

--- src/checker/checker.js
+++ src/checker/checker.js
@@ -787,6 +787,7 @@ try {
         }
         const li = document.createElement('li');
         li.classList.add(statusLabel.replace(/\s+/g, '-').toLowerCase());
+        li.style.color = (correctCount === totalQuestions) ? 'mediumseagreen' : (attemptedCount === totalQuestions) ? 'royalblue' : ((segment.due && (new Date(`${segment.due}T00:00:00`).getTime() < new Date().setHours(0, 0, 0, 0))) ? ((attemptedCount === 0) ? 'indianred' : 'darkorange') : '');
         li.innerHTML = `${icon} <b>${segment.number} - ${segment.name.length > 50 ? segment.name.substring(0, 50 - 3).trim() + '...' : segment.name}:</b><p>${statusLabel}</p><span style="float: right; font-weight: 600;">${attemptedCount}/${totalQuestions} Answered • ${correctCount}/${totalQuestions} Correct (${(totalQuestions > 0) ? Math.round((correctCount / totalQuestions) * 100) : 0}%)</span>`;
         document.getElementById("segments-completed").querySelector('ul').append(li);
         li.addEventListener('click', () => {
